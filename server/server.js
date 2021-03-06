b40825f2735122f8ea92fb744a28bf0ef9176e49
jjm2317 <wlaks2317@gmail.com>
2021-03-06 15:53:31 +0900

    feat: test Get api

--- server/server.js
+++ server/server.js
@@ -1,13 +1,20 @@
 // npm install express
 
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-    res.send('Server Response Success');
-})
+const testData = { a: 1, b: 2, c: 3 };
+app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("Server Response Success");
+});
+app.get("/test", (req, res) => {
+  console.log("start");
+  res.send(JSON.stringify(testData));
+});
 app.listen(PORT, () => {
-    console.log(`Server On : http://localhost:${PORT}/`);
-})
+  console.log(`Server On : http://localhost:${PORT}/`);
+});
