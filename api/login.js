634b9638b050820348fd8a12e12d8f7460124be1
jjm2317 <wlaks2317@gmail.com>
2021-03-19 22:00:13 +0900

    fix: remove json server

--- api/login.js
+++ api/login.js
@@ -1,5 +1,5 @@
 //userTest, db로 구현 필요
-const users = [{ username: "green", password: "123123" }];
+const users = [{ username: "green@gmail.com", password: "123123" }];
 
 const login = (request, response) => {
   const { username, password } = request.body;
@@ -7,6 +7,7 @@ const login = (request, response) => {
     (user) => user.username === username && user.password === password
   );
   user !== undefined ? response.status(200) : response.status(400);
+  console.log(JSON.stringify(user));
   response.json(JSON.stringify(user));
 };
 
