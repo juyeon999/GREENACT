324468bad2607c59e38dc5c54fdb00ca7c5c6eb3
Song_a <o37o1108@gmail.com>
2021-03-27 13:53:42 +0900

    [Add]

--- api/db.js
+++ api/db.js
@@ -1,37 +1,19 @@
-// var mysql = require('mysql');
-// var connection = {
-//     host : 'localhost',
-//     user : 'root',
-//     password : '1111',//     password : '000930', database : 'sep_collection'
-//     database : 'nodedb'
-// }
-//
-// module.exports = {
-//     //DB와 서버간의 연결 객체를 반환하는 'init()' 함수
-//     init: function () {
-//         return mysql.createConnection(connection);
-//     },
-//     //데이터 교환을 위해 연결을 시키는 'connect()' 함수
-//     connect: function(conn) {
-//         conn.connect(function(err) {
-//             if(err) console.error('mysql connection error : ' + err);
-//             else console.log('mysql is connected successfully!');
-//         });
-//         conn.query('SELECT * From general_trash', function(err,results,fields) {
-//             if(err) {
-//                 console.log(err);
-//             }
-//             console.log(results);
-//         });
-//     }
-// }
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : '000930',
+    database : 'sep_collection'
+});
 
-// connection.connect();
-//
-// connection.query('SELECT * From general_trash', function(err,results,fields) {
-//     if(err) {
-//         console.log(err);
-//     }
-//     console.log(results);
-// });
-// connection.end();
+connection.connect();
+
+
+connection.query('SELECT * From general_trash', function(err,results,fields) {
+    if(err) {
+        console.log(err);
+    }
+    console.log(results);
+});
+
+connection.end();
